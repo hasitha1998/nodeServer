b19879e31b58a6b936c421f126a4c4b16680aaf8
Hasitha Sanjaya <79785680+hasitha1998@users.noreply.github.com>
2023-12-21 10:29:07 +0530

    fully completed node backend server

--- server.js
+++ server.js
@@ -9,6 +9,30 @@ app.use(express.json())
     res.send("hello node")
  })
 
+ app.get('/products',async(req,res)=>{
+    try {
+        const product = await Product.find({})
+        res.status(200).json(product)
+        
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }
+ })
+
+ app.get('/products/:id',async(req,res)=>{
+
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product)
+        
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }
+ })
+
  app.post('/product',async (req,res)=>{
     try{
         const product = await Product.create(req.body)
@@ -19,6 +43,36 @@ app.use(express.json())
         res.status(500).json({message: error.message})
     }
  })
+
+ //update
+app.put('/products/:id',async (req,res)=>{
+    try {
+        const{id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+//delete
+app.delete('/product/:id', async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message : `cannot find any product with ID ${id}`})
+        }
+        
+    res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
 mongoose.connect('mongodb+srv://hasithasanjaya123:hasitha1234@cluster0.xqmbzbd.mongodb.net/')
 .then(()=>{
